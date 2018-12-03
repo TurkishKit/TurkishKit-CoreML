d9606e622d8dc54e23c195d7411bf2719326714c
Berkin Ceylan <selhanberkinceylan@hotmail.com>
2018-12-04 00:10:26 +0300

    Initial Commit

---
+++ TKCoreML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TKCoreML
+//
+//  Created by Berkin Ceylan on 4.12.2018.
+//  Copyright © 2018 Berkin Ceylan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
